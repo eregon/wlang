6141e4d043c6e9bfde8685932bb27da619352a64
Bernard Lambeau <blambeau@gmail.com>
2012-01-12 18:44:40 +0100

    Factorize test creator

--- test/standard_dialects/test_all.rb
+++ test/standard_dialects/test_all.rb
@@ -4,18 +4,21 @@ require 'wlang'
 require 'test/unit'
 module WLang
   class StandardDialectsTest < Test::Unit::TestCase
+
+    def self.define_method_for(folder, template_file)
+      dialect_name = File.basename(folder)
+      basename = File.basename(template_file, ".tpl")
+
+      define_method("test_#{dialect_name}_#{basename}") do 
+        expected = File.read(File.join(folder, "#{basename}.exp"))
+        template = WLang::file_template(template_file, "wlang/#{dialect_name}")
+        assert_equal(expected, template.instantiate)
+      end
+    end
     
     Dir["#{File.dirname(__FILE__)}/*"].each do |folder|
-      dialect_name = File.basename(folder)
       Dir["#{folder}/*.tpl"].each do |template_file|
-        basename = File.basename(template_file, ".tpl")
-        
-        define_method("test_#{dialect_name}_#{basename}") do 
-          expected = File.read(File.join(folder, "#{basename}.exp"))
-          template = WLang::file_template(template_file, "wlang/#{dialect_name}")
-          assert_equal(expected, template.instantiate)
-        end
-
+        define_method_for(folder, template_file)
       end
     end
     
