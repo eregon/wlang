5d455f985a505faac09aa40c7125e666197beafe
Bernard Lambeau <blambeau@gmail.com>
2012-01-12 18:39:01 +0100

    Define one test per case (standard dialects)

--- test/standard_dialects/test_all.rb
+++ test/standard_dialects/test_all.rb
@@ -5,23 +5,17 @@ require 'test/unit'
 module WLang
   class StandardDialectsTest < Test::Unit::TestCase
     
-    def test_standard_dialects
-      Dir["#{File.dirname(__FILE__)}/*"].each do |folder|
-        dialect_name = File.basename(folder)
-        Dir["#{folder}/*.tpl"].each do |template_file|
-          begin
-            basename = File.basename(template_file, ".tpl")
-            expected = File.read(File.join(folder, "#{basename}.exp"))
-            template = WLang::file_template(template_file, "wlang/#{dialect_name}")
-            assert_equal(expected, template.instantiate, "Standard dialect test failed wlang/#{basename}")
-          rescue WLang::Error => ex
-            puts "Standard dialect test failed: #{template_file}\n#{ex.message}"
-            puts ex.wlang_backtrace.join("\n")
-          rescue Exception => ex
-            puts "Standard dialect test failed: #{template_file}\n#{ex.message}"
-            puts ex.backtrace.join("\n")
-          end
+    Dir["#{File.dirname(__FILE__)}/*"].each do |folder|
+      dialect_name = File.basename(folder)
+      Dir["#{folder}/*.tpl"].each do |template_file|
+        basename = File.basename(template_file, ".tpl")
+        
+        define_method("test_#{dialect_name}_#{basename}") do 
+          expected = File.read(File.join(folder, "#{basename}.exp"))
+          template = WLang::file_template(template_file, "wlang/#{dialect_name}")
+          assert_equal(expected, template.instantiate)
         end
+
       end
     end
     
